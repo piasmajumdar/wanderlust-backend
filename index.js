6faa1f0c3c3a12af9779e8a643389246e979e676
piasmajumdar <piasmajumdar@gmail.com>
2026-05-18 11:28:50 +0530

    get all destinations data

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors')
+
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
 
@@ -8,6 +10,9 @@ const uri = process.env.MONGODB_URI;
 const app = express();
 const PORT  = process.env.PORT;
 
+//middlewar
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -18,14 +23,29 @@ const client = new MongoClient(uri, {
 });
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+
+    const db = client.db("wanderlust");
+    const destinationCollection = db.collection('destination');
+
+
+    app.get('/destinations', async(req, res)=>{
+        const result =await destinationCollection.find().toArray();
+        res.json(result);
+    })
+
+    app.post('/destination', async(req, res)=>{
+        const destination = req.body;
+        console.log(destination)
+        const result = await destinationCollection.insertOne(destination);
+        res.json(result);
+    })
+
+
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
